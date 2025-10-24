c536c4f67f122840fc1c444a6deeb92830d007ee
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2025-10-24 03:59:04 -0700

    Add files via upload

---
+++ qemu/build-iso.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+KERNEL_ARG='--live-karg-append=coreos.liveiso.fromram'
+IGNITION_ARG='--live-ignition=./config.ign'
+podman run --security-opt label=disable --pull=always --rm -v .:/data -w /data     quay.io/coreos/coreos-installer:release iso customize --live-karg-append=coreos.liveiso.fromram --live-ignition=./config.ign     -o customized.iso fedora-coreos-42.20250914.3.0-live-iso.x86_64.iso
