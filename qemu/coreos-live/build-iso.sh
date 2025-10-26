78a7c3eb6d276a810942996d45b40ad728487329
dev@dashxd <125025267+dash-xd@users.noreply.github.com>
2025-10-25 18:13:43 -0700

    Update build-iso.sh

--- qemu/coreos-live/build-iso.sh
+++ qemu/coreos-live/build-iso.sh
@@ -2,4 +2,4 @@
 
 KERNEL_ARG='--live-karg-append=coreos.liveiso.fromram'
 IGNITION_ARG='--live-ignition=./config.ign'
-podman run --security-opt label=disable --pull=always --rm -v .:/data -w /data     quay.io/coreos/coreos-installer:release iso customize --live-karg-append=coreos.liveiso.fromram --live-ignition=./config.ign     -o customized.iso fedora-coreos-42.20250914.3.0-live-iso.x86_64.iso
+podman run --security-opt label=disable --pull=always --rm -v .:/data -w /data     quay.io/coreos/coreos-installer:release iso customize --live-karg-append=coreos.liveiso.fromram --live-ignition=./config.ign     -o ignited.iso fedora-coreos-42.20250914.3.0-live-iso.x86_64.iso
